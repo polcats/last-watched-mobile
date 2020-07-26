1fad58b9ecbb81161f0a18224ab9e73b43c91d46
Paul Catalan <24451518+polcats@users.noreply.github.com>
2020-07-26 13:33:00 +0800

    Changed title for tabs.

--- src/screens/stacks/Home.tsx
+++ src/screens/stacks/Home.tsx
@@ -9,9 +9,27 @@ const Home: React.FC<HomeScreenNavigation> = ({ route, navigation }) => {
   const context = useContext(appContext);
   return (
     <Tab.Navigator>
-      <Tab.Screen name="OnGoing" component={OnGoing} />
-      <Tab.Screen name="OnHold" component={OnHold} />
-      <Tab.Screen name="Done" component={Done} />
+      <Tab.Screen
+        name="OnGoing"
+        options={({ navigation }) => ({
+          title: 'On Going',
+        })}
+        component={OnGoing}
+      />
+      <Tab.Screen
+        name="OnHold"
+        options={({ navigation }) => ({
+          title: 'On Hold',
+        })}
+        component={OnHold}
+      />
+      <Tab.Screen
+        name="Done"
+        options={({ navigation }) => ({
+          title: 'Finished',
+        })}
+        component={Done}
+      />
     </Tab.Navigator>
   );
 };
