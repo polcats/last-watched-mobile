fc44296d170565c634bff48e5c535672415ec72f
Paul Catalan <24451518+polcats@users.noreply.github.com>
2020-07-27 09:02:37 +0800

    Moved the list preloader.

--- App.tsx
+++ App.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Screen } from './src/screens/';
 import { AppLoading } from 'expo';
 import { useFonts } from 'expo-font';
+import { AsyncStorage } from 'react-native';
+import { appContext } from './src/models';
 
 export default function App() {
+  const context = useContext(appContext);
+
+  (async () => {
+    try {
+      const data = await AsyncStorage.getItem('lastWatchedData');
+      const items = data ? JSON.parse(data) : undefined;
+      if (items) {
+        items.forEach((value: any) => {
+          context.shows.addItem(value[1].$);
+        });
+      }
+    } catch (error) {}
+  })();
+
   let [fontsLoaded] = useFonts({
     Ubuntu: require('./assets/fonts/Ubuntu-L.ttf'),
   });
